d97d384ae8b15975bb98263652ac0638d12f16dd
shadrack512 <shadrackmunyao661@gmail.com>
2025-08-03 13:59:35 +0300

    updated codes

---
+++ firebase/firebase-admin-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCraXq51qucrTWJlD7ZXjUodP92Aqxfh3c",
+  authDomain: "munyaotech-lab.firebaseapp.com",
+  projectId: "munyaotech-lab",
+  storageBucket: "munyaotech-lab.appspot.com",
+  messagingSenderId: "654834051986",
+  appId: "1:654834051986:web:076f2f5c10cae1291baf4b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
